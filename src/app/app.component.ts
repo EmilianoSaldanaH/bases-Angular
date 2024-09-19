c8d030ff50b9b28ce4c04092575d562e45838136
Juan Emiliano Saldaña Hernandez <soyemiliano25@hotmail.com>
2024-09-18 21:01:40 -0600

    primer commit miercoles 18

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,21 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'bases-angular';
+
+  dobleNumero(x: number): number {
+    return x * 2;
+  }
+
+  alumno = {
+    nombre: 'Juan',
+    edad: 25,
+    curso: 'Angular',
+    fechaInscrito: new Date(),
+    pago: 1000, // Definido el pago
+    page: 2220
+  };
 }
---
+++ src/app/product/product-list/product-list.component.ts
@@ -0,0 +1,45 @@
+import { Component } from '@angular/core';
+ 
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrl: './product-list.component.css'
+})
+export class ProductListComponent {
+ 
+ 
+  productos:any[]=[
+    {
+      "productoId":1,
+      "Modelo":"Sentra",
+      "Descripcion":"4 puertas",
+      "Precio":2000,
+      "Year":2023,
+      "Marca":"NISSAN",
+      "Color":"Azul",
+      "ImagenUrl":""
+    },
+    {
+      "productoId":3,
+      "Modelo":"Rio",
+      "Descripcion":"4 puertas",
+      "Precio":150000,
+      "Year":2020,
+      "Marca":"KIA",
+      "Color":"Azul",
+      "ImagenUrl":""
+    },
+    {
+      "productoId":3,
+      "Modelo":"Rio",
+      "Descripcion":"4 puertas",
+      "Precio":150000,
+      "Year":2020,
+      "Marca":"KIA",
+      "Color":"Azul",
+      "ImagenUrl":""
+    }
+  ]
+ 
+}
+ 
