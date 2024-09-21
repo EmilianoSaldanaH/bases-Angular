de32ea005ecbcd6e8ae92212c361fd63c4e66c36
Juan Emiliano Saldaña Hernandez <soyemiliano25@hotmail.com>
2024-09-21 10:15:05 -0600

    Commit dia sabado 21 de semptiembre

--- src/app/product/product-list/product-list.component.ts
+++ src/app/product/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {IProductos } from '../productos';
  
 @Component({
   selector: 'app-product-list',
@@ -7,16 +8,22 @@ import { Component } from '@angular/core';
 })
 export class ProductListComponent {
 
-  //*ngIf-true
- 
- 
-  productos:any[]=[
+imageWidth:number=60;
+imageMargin:number=2;
+muestraImg:boolean=true;
+listFilter:string=''
+
+muestImg(): void{
+  this.muestraImg = !this.muestraImg;
+}
+
+  productos:IProductos[]=[
     {
       "productoId":1,
       "Modelo":"Sentra",
       "Descripcion":"4 puertas",
       "Precio":2000,
-      "Year":2023,
+      "Year":" septiembre 1 2023",
       "Marca":"NISSAN",
       "Color":"Azul",
       "ImagenUrl":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTHirhyuXF6MfbmNSaPhPWjXewZ45QJhbYA7w&s"
@@ -26,7 +33,7 @@ export class ProductListComponent {
       "Modelo":"Rio",
       "Descripcion":"4 puertas",
       "Precio":150000,
-      "Year":2020,
+      "Year": "marzo 23 2020",
       "Marca":"KIA",
       "Color":"Azul",
       "ImagenUrl":"https://static.vecteezy.com/system/resources/thumbnails/023/192/562/small/sport-car-running-on-the-road-in-future-city-created-with-generative-ai-free-photo.jpg"
@@ -36,7 +43,7 @@ export class ProductListComponent {
       "Modelo":"Rio",
       "Descripcion":"4 puertas",
       "Precio":150000,
-      "Year":2020,
+      "Year": "december 10 2018",
       "Marca":"KIA",
       "Color":"Azul",
       "ImagenUrl":"https://img.freepik.com/foto-gratis/coche-3d-colores-vibrantes_23-2150796964.jpg"
