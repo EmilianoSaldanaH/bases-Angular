e7b18a04f45aac8be629c399d80d7dafe4695c20
Juan Emiliano Saldaña Hernandez <soyemiliano25@hotmail.com>
2024-09-20 20:53:03 -0600

    Commit del viernes 20 de septiembre

--- src/app/product/product-list/product-list.component.ts
+++ src/app/product/product-list/product-list.component.ts
@@ -6,6 +6,8 @@ import { Component } from '@angular/core';
   styleUrl: './product-list.component.css'
 })
 export class ProductListComponent {
+
+  //*ngIf-true
  
  
   productos:any[]=[
@@ -17,7 +19,7 @@ export class ProductListComponent {
       "Year":2023,
       "Marca":"NISSAN",
       "Color":"Azul",
-      "ImagenUrl":""
+      "ImagenUrl":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTHirhyuXF6MfbmNSaPhPWjXewZ45QJhbYA7w&s"
     },
     {
       "productoId":3,
@@ -27,7 +29,7 @@ export class ProductListComponent {
       "Year":2020,
       "Marca":"KIA",
       "Color":"Azul",
-      "ImagenUrl":""
+      "ImagenUrl":"https://static.vecteezy.com/system/resources/thumbnails/023/192/562/small/sport-car-running-on-the-road-in-future-city-created-with-generative-ai-free-photo.jpg"
     },
     {
       "productoId":3,
@@ -37,7 +39,7 @@ export class ProductListComponent {
       "Year":2020,
       "Marca":"KIA",
       "Color":"Azul",
-      "ImagenUrl":""
+      "ImagenUrl":"https://img.freepik.com/foto-gratis/coche-3d-colores-vibrantes_23-2150796964.jpg"
     }
   ]
  
